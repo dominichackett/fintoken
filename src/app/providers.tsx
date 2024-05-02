a69a5ac2185d5883bb00cbe7a50c88265a184e8a
Dominic Leon Hackett <dominichackett@gmail.com>
2024-05-02 18:54:59 -0400

    Initial Commit

---
+++ src/app/providers.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import * as React from 'react';
+import {Chain,
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit';
+import {
+  argentWallet,
+  trustWallet,
+  ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import {
+  scrollSepolia,zkSyncTestnet,avalancheFuji,
+} from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+
+const { wallets } = getDefaultWallets();
+const Amoy: Chain = {
+  id: 80002,
+  name: 'Polygon Amoy',
+  network: 'Polygon Amoy',
+  iconBackground: '#3e6957',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'MATIC',
+    symbol: 'MATIC',
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc-amoy.polygon.technology'] },
+    default: { http: ['https://rpc-amoy.polygon.technology'] },
+  },
+  blockExplorers: {
+    default: { name: 'OKLINK', url: 'https://www.oklink.com/amoy' },
+    etherscan: { name: 'OKLINK', url: 'https://www.oklink.com/amoy' },
+  },
+
+  testnet: true,
+};
+
+const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [argentWallet, trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [
+    Amoy,scrollSepolia,zkSyncTestnet,avalancheFuji,
+    
+  ],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
